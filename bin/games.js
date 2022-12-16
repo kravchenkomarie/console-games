4509629a9bacb2d2618d8381741d6d0f26bf9e83
Maria Kravchenko <kravchenkomarie@gmail.com>
2022-12-16 15:38:23 +0300

    connecting a library to read user input

--- bin/games.js
+++ bin/games.js
@@ -1,3 +1,11 @@
 #!/usr/bin/env node
 
+import readlineSync from 'readline-sync';
+
 console.log('Welcome to the Games!');
+console.log('May I have your name?');
+
+const name = readlineSync.question('Your answer: ');
+
+console.log(`Hello, ${name}!`)
+
