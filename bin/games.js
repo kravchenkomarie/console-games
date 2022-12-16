e3a93dddc1411db53f06346f18c01a9d49bb5115
Maria Kravchenko <kravchenkomarie@gmail.com>
2022-12-16 18:02:08 +0300

    first task

--- bin/games.js
+++ bin/games.js
@@ -8,3 +8,30 @@ console.log('May I have your name?');
 const name = readlineSync.question('Your answer: ');
 
 console.log(`Hello, ${name}!`);
+
+console.log('Answer "yes" if the number is even, otherwise answer "no".');
+
+const getRandomNumber = () => Math.round(Math.random()*100);
+
+console.log('Question: ', getRandomNumber());
+
+// function isEven(getRandomNumber) { return getRandomNumber % 2 === 0; }
+
+
+const answer = readlineSync.question('Your answer: ');
+
+function checkTheAnswer () {
+    if (getRandomNumber % 2 === 0 && answer == 'yes') {
+            return 'Correct!' 
+    }
+    }   else if (getRandomNumber % 2 !== 0 && answer == 'yes') {
+            return `'yes' is wrong answer ;(. Correct answer was 'no'.
+            Let's try again, ${name}!`
+        } else if (getRandomNumber % 2 !== 0 && answer == 'no') {
+            return 'Correct!'
+        } else {
+            return 'Something went wrong! Try again <3'
+        }
+
+
+console.log(checkTheAnswer(answer));
