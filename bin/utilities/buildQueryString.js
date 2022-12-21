f6590ba2284c573d4ffb62cf603e0be134b893d6
Maria Kravchenko <kravchenkomarie@gmail.com>
2022-12-21 18:04:39 +0300

    add bqs function file

--- bin/utilities/buildQueryString.js
+++ bin/utilities/buildQueryString.js
@@ -1,7 +1,5 @@
-
-
-
-
-function buildQueryString (params) {
-    
+export const bqs = (params) => {
+    const allParams = new URLSearchParams(params);
+    allParams.sort();
+    return allParams.toString();
 }
